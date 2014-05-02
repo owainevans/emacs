205367cc56d897b7419962b09465ea94d1583804
Owain Evans <owain@mit.edu>
2014-05-02 13:20:54 -0400

    compareSpeed has extra arg for amount of inference

--- .emacs
+++ .emacs
@@ -1,11 +1,99 @@
+;; path where settings files are kept
+(add-to-list 'load-path "~/.emacs.d/settings")
+
+;; global config variables
+(setq plugin-path "~/.emacs.d/plugins/")
+(setq elget-path "~/.emacs.d/el-get/")
+
+;; various generic/global config
+(require 'custom-functions)
+(require 'general-settings)
 
 
+;---------------;
+;;; Utilities ;;;
+;---------------;
 
+;; Jedi stuff
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:setup-keys t)                      ; optional
+(setq jedi:complete-on-dot t)                 ; optional
+;(require 'jedi)
+
+;; Popup
+(include-elget-plugin "popup")
+(require 'popup)
+
+;; Websocket
+(include-plugin "websocket")
+(require 'websocket)
+
+;; Request
+(include-plugin "request")
+(require 'request)
+
+;; Auto complete
+(require 'auto-complete-settings)
+
+;; Camelcase functions
+;(require 'camelcase-settings)
+
+
+;----------------------;
+;;; Standalone tools ;;;
+;----------------------;
+
+;; el-get
+(include-plugin "el-get")
+(require 'el-get)
+
+;; Git
+(include-plugin "magit")
+(require 'magit)
+
+
+;-----------;
+;;; Modes ;;;
+;-----------;
+
+;; Ido mode
+(require 'ido)
+(ido-mode 1)
+
+;; MuMaMo
+(require 'mumamo-settings)
+
+;; Markdown mode
+(require 'markdown-settings)
+
+;; Python mode 
+(require 'python-settings)
+
+;; LaTeX and Auctex
+(require 'latex-settings)
+
+;; SCSS Mode
+(require 'scss-settings)
+
+;; Matlab mode
+(require 'matlab-settings)
+
+
+;---------------------------------------------------------------------
+;; Put auto 'custom' changes in a separate file (this is stuff like
+;; custom-set-faces and custom-set-variables)
+;(load 
+ ;(setq custom-file (expand-file-name "settings/custom.el" user-emacs-directory))
+ ;'noerror)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(custom-enabled-themes (quote (tsdh-dark)))
+ '(show-paren-mode t)
+ '(size-indication-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -13,32 +101,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal)))))
-; (autoload 'python-mode "python-mode" "Python Mode." t)
-; (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
-; (add-to-list 'interpreter-mode-alist '("python" . python-mode))
-
-
-; ipython as default interpreter
-(require 'python)
-(setq python-shell-interpreter "ipython")
-(setq python-shell-interpreter-args "--pylab")
-
-
-
-;; el-get stuff
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
-
-;; Jedi stuff
-(add-hook 'python-mode-hook 'jedi:ac-setup)
-(setq jedi:setup-keys t)
-(add-hook 'python-mode-hook 'jedi:setup)
--- .emacs.d/el-get/.loaddefs.el
+++ .emacs.d/el-get/.loaddefs.el
@@ -4,7 +4,7 @@
 
 
 ;;;### (autoloads (auto-complete) "auto-complete/auto-complete" "auto-complete/auto-complete.el"
-;;;;;;  (21213 41750 676000 0))
+;;;;;;  (21216 18945 224000 0))
 ;;; Generated autoloads from auto-complete/auto-complete.el
 
 (autoload 'auto-complete "auto-complete/auto-complete" "\
@@ -14,64 +14,9 @@ Start auto-completion at current point.
 
 ;;;***
 
-;;;### (autoloads (el-get-checksum el-get-make-recipes el-get-cd
-;;;;;;  el-get-self-update el-get-update-packages-of-type el-get-update-all
-;;;;;;  el-get-version) "el-get/el-get" "el-get/el-get.el" (21213
-;;;;;;  41516 716000 0))
-;;; Generated autoloads from el-get/el-get.el
-
-(autoload 'el-get-version "el-get/el-get" "\
-Message the current el-get version
-
-\(fn)" t nil)
-
-(autoload 'el-get-update-all "el-get/el-get" "\
-Performs update of all installed packages.
-
-\(fn &optional NO-PROMPT)" t nil)
-
-(autoload 'el-get-update-packages-of-type "el-get/el-get" "\
-Update all installed packages of type TYPE.
-
-\(fn TYPE)" t nil)
-
-(autoload 'el-get-self-update "el-get/el-get" "\
-Update el-get itself.  The standard recipe takes care of reloading the code.
-
-\(fn)" t nil)
-
-(autoload 'el-get-cd "el-get/el-get" "\
-Open dired in the package directory.
-
-\(fn PACKAGE)" t nil)
-
-(autoload 'el-get-make-recipes "el-get/el-get" "\
-Loop over `el-get-sources' and write a recipe file for each
-entry which is not a symbol and is not already a known recipe.
-
-\(fn &optional DIR)" t nil)
-
-(autoload 'el-get-checksum "el-get/el-get" "\
-Compute the checksum of the given package, and put it in the kill-ring
-
-\(fn PACKAGE &optional PACKAGE-STATUS-ALIST)" t nil)
-
-;;;***
-
-;;;### (autoloads (el-get-list-packages) "el-get/el-get-list-packages"
-;;;;;;  "el-get/el-get-list-packages.el" (21213 41516 708000 0))
-;;; Generated autoloads from el-get/el-get-list-packages.el
-
-(autoload 'el-get-list-packages "el-get/el-get-list-packages" "\
-Display a list of packages.
-
-\(fn)" t nil)
-
-;;;***
-
 ;;;### (autoloads (jedi:setup anything-jedi-related-names helm-jedi-related-names
 ;;;;;;  jedi:ac-setup jedi:complete jedi:start-dedicated-server)
-;;;;;;  "jedi/jedi" "jedi/jedi.el" (21213 41755 692000 0))
+;;;;;;  "jedi/jedi" "jedi/jedi.el" (21216 18972 8000 0))
 ;;; Generated autoloads from jedi/jedi.el
 
 (autoload 'jedi:start-dedicated-server "jedi/jedi" "\
@@ -138,14 +83,9 @@ what jedi can do.
 ;;;;;;  "auto-complete/auto-complete-pkg.el" "ctable/ctable.el" "ctable/test-ctable.el"
 ;;;;;;  "deferred/concurrent-sample.el" "deferred/concurrent.el"
 ;;;;;;  "deferred/deferred-samples.el" "deferred/deferred.el" "deferred/test-concurrent.el"
-;;;;;;  "deferred/test-deferred.el" "el-get/el-get-autoloads.el"
-;;;;;;  "el-get/el-get-build.el" "el-get/el-get-byte-compile.el"
-;;;;;;  "el-get/el-get-core.el" "el-get/el-get-custom.el" "el-get/el-get-dependencies.el"
-;;;;;;  "el-get/el-get-install.el" "el-get/el-get-methods.el" "el-get/el-get-notify.el"
-;;;;;;  "el-get/el-get-recipes.el" "el-get/el-get-status.el" "epc/epc.el"
-;;;;;;  "epc/epcs.el" "epc/test-epc.el" "fuzzy/fuzzy.el" "jedi/jedi-pkg.el"
-;;;;;;  "jedi/test-jedi.el" "jedi/tryout-jedi.el" "popup/popup.el")
-;;;;;;  (21213 41765 942091 22000))
+;;;;;;  "deferred/test-deferred.el" "epc/epc.el" "epc/epcs.el" "epc/test-epc.el"
+;;;;;;  "fuzzy/fuzzy.el" "jedi/jedi-pkg.el" "jedi/test-jedi.el" "jedi/tryout-jedi.el"
+;;;;;;  "popup/popup.el") (21216 18983 878690 16000))
 
 ;;;***
 
